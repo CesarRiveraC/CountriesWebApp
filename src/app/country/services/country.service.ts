a965a9fb4cb02bc0ad2a245a8f12074f08919ffb
CesarRiveraC <cesarrivcam@gmail.com>
2022-01-14 16:46:24 -0600

    changes to consume new version of the countries API

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -7,35 +7,40 @@ import { Country } from '../interfaces/country.interface';
   providedIn: 'root',
 })
 export class CountryService {
-  private apiBaseUrl: string = 'https://restcountries.eu/rest/v2';
+  private apiBaseUrl: string = 'https://restcountries.com/v2';
 
-  get httpParams(){
-    return new HttpParams().set(
-        'fields',
-        'name;capital;alpha2Code;flag;population'
-      );
-  }
+  // get httpParams(){
+  //   return new HttpParams().set(
+  //       'fields',
+  //       'name;capital;alpha2Code;alpha3Code;flag;population'
+  //     );
+  // }
 
   constructor(private http: HttpClient) {}
 
   searchCountry(country: string): Observable<Country[]> {
     const apiUrl = `${this.apiBaseUrl}/name/${country}`;
-    return this.http.get<Country[]>(apiUrl, {params:this.httpParams});
+    // return this.http.get<Country[]>(apiUrl, {params:this.httpParams});
+    return this.http.get<Country[]>(apiUrl);
   }
 
   searchCapital(capital: string): Observable<Country[]> {
     const apiUrl = `${this.apiBaseUrl}/capital/${capital}`;
-    return this.http.get<Country[]>(apiUrl,{params:this.httpParams});
+    // return this.http.get<Country[]>(apiUrl,{params:this.httpParams});
+    return this.http.get<Country[]>(apiUrl);
+
   }
 
   getCountryByCode(code: string): Observable<Country> {
     const apiUrl = `${this.apiBaseUrl}/alpha/${code}`;
+    //const apiUrl = `${this.apiBaseUrl}/alpha/CO`;
     return this.http.get<Country>(apiUrl);
   }
 
   getCountriesByRegion(region: string): Observable<Country[]> {
-    
     const apiUrl = `${this.apiBaseUrl}/region/${region}`;
-    return this.http.get<Country[]>(apiUrl, { params: this.httpParams });
+    //return this.http.get<Country[]>(apiUrl, { params: this.httpParams });
+    return this.http.get<Country[]>(apiUrl);
+
   }
 }
